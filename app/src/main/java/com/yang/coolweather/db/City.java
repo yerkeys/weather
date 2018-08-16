4b0799350d0c2253c70b8b9bf215e3ce177914ba
yang <1920282351@qq.com>
2018-08-16 19:49:25 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/yang/coolweather/db/City.java
@@ -0,0 +1,43 @@
+package com.yang.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityCode;
+    private String cityName;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
